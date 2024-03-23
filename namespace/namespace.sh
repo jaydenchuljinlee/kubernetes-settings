edba47f23c0fdef823fa9e3f053f949285ea48f6
cheoljin <cheoljin1234@naver.com>
2024-03-23 18:37:41 +0900

    [네임스페에스] - 생성 명령어 추가

---
+++ namespace/namespace.sh
@@ -0,0 +1,5 @@
+kubectl create namespace dev-external
+kubectl create namespace dev-deployemnt
+kubectl create namespace dev-service1
+kubectl create namespace dev-service2
+kubectl create namespace dev-repository
